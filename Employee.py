5b475e91edff18aea714d765a5ab18224cdfd151
SahanaLakshmipathi <168077707+SahanaLakshmipathi@users.noreply.github.com>
2024-06-12 18:05:48 +0530

    Delete Employee.py

--- Employee.py
+++
@@ -1,9 +0,0 @@
-class EmployeeDetails:
-    def __init__(self,name, id, category,amount):
-        self.name = name
-        self.employee_ID = id
-        self.category = category
-        self.amount = amount
-#instead of printing memory address we can add details to show in string
-    def __repr__(self):
-        return f"<Expense: {self.name},{self.employee_ID},{self.category},{self.amount: .2f}>"
